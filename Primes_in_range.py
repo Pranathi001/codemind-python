1b0c371067ed87c26ad0bf19e251c7b5d7baaf3a
~pranathi~ <96736864+Pranathi001@users.noreply.github.com>
2022-06-29 20:01:40 +0530

    def prime_range(x,y):     count=0     for i in range(x,y+1):         if i>1:             for j in range(2,int(i**0.5)+1):                 if i%j==0:                     break             else:                     count+=1     print(count) x=int(input()) y=int(input()) prime_range(x,y)

--- Primes_in_range.py
+++ Primes_in_range.py
@@ -1,14 +1,13 @@
-def prime(a):
-    if a==1:
-        return False
-    for i in range(2,int(a**0.5)+1):
-        if a%i==0:
-            return False
-    return True
-m=int(input())
-n=int(input())
-c=0
-for i in range(m,n+1):
-    if prime(i):
-        c+=1
-print(c)
+def prime_range(x,y):
+    count=0
+    for i in range(x,y+1):
+        if i>1:
+            for j in range(2,int(i**0.5)+1):
+                if i%j==0:
+                    break
+            else:
+                    count+=1
+    print(count)
+x=int(input())
+y=int(input())
+prime_range(x,y)
