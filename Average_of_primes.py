1cf0571ccf7c94a670025318d7b4c08eb70bd6d9
~pranathi~ <96736864+Pranathi001@users.noreply.github.com>
2022-06-30 10:22:32 +0530

    def prime(n):     for i in range(2,int(n**0.5)+1):         if n%i==0:             return False     else:         return True n=int(input()) a=list(map(int,input().split())) sum=0 c=0 for i in a:     if i>1:         if prime(i):             sum=sum+i             c+=1 avg=sum/c print("%.2f"%avg)

---
+++ Average_of_primes.py
@@ -0,0 +1,17 @@
+def prime(n):
+    for i in range(2,int(n**0.5)+1):
+        if n%i==0:
+            return False
+    else:
+        return True
+n=int(input())
+a=list(map(int,input().split()))
+sum=0
+c=0
+for i in a:
+    if i>1:
+        if prime(i):
+            sum=sum+i
+            c+=1
+avg=sum/c
+print("%.2f"%avg)
